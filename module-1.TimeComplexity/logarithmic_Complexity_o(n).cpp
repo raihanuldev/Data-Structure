f196c20b0bd1a67459bab3f6fdd0d0d603e5d93c
raihanuldev <rihanthedev@gmail.com>
2024-12-07 23:55:32 +0600

    log(N)

---
+++ module-1.TimeComplexity/logarithmic_Complexity_o(n).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >>n;
+    for(int i =0; i<n; i*=2) //log(n)
+    {
+        ///
+    }
+    //explain in khataaa
+    return 0;
+}
