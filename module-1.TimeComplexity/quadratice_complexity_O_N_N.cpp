00e91241bae6ee36f3557c8610f949fe5173cdfa
raihanuldev <rihanthedev@gmail.com>
2024-12-08 01:34:33 +0600

    quadractce_Complexity

---
+++ module-1.TimeComplexity/quadratice_complexity_O_N_N.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >>n;
+    for(int i =0; i<n;i++) // O(N)
+    {
+        for(int j =0; j<n;j++) // O(N)
+        {
+            cout <<"Hello "<<endl;
+        }
+    }
+    //SO There complexity O(N*N) => N square
+    return 0;
+}
