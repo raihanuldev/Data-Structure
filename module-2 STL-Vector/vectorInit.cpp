a90fc605e22d1f0e154cc97fac21cf1d3c2eb6ed
raihanuldev <rihanthedev@gmail.com>
2024-12-09 14:30:07 +0600

    vector init

---
+++ module-2 STL-Vector/vectorInit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // vector<int>v; //type 1
+    // vector<int>v(10); 
+    vector<int>v(10,9); //n and value pass
+    vector<int>v2(v); //copy v element
+    vector<int>v3 = {2,3,4,5,3};
+    
+    
+    cout <<v.size();
+    return 0;
+}
