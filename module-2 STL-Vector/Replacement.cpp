38514687d11fbe0175a114c7135afa7f20ed4729
raihanuldev <rihanthedev@gmail.com>
2024-12-10 17:33:16 +0600

    counting

---
+++ module-2 STL-Vector/Replacement.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cin>>n;
+    vector<int>v(n);
+    for(int i =0; i<n; i++){
+        int x; cin>>x;
+        if(x==0){
+            v[i]=x;
+        }else if(x>0){
+            v[i] = 1;
+        }
+        else{
+            v[i] = 2;
+        }
+    }
+    for(int i =0; i<n; i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
