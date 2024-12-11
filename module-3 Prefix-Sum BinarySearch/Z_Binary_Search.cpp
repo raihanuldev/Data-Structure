345a1002a6954c8940342cd123ea02f326a75161
raihanuldev <rihanthedev@gmail.com>
2024-12-11 20:28:41 +0600

    bianry serach

--- module-3 Prefix-Sum BinarySearch/Z_Binary_Search.cpp
+++ module-3 Prefix-Sum BinarySearch/Z_Binary_Search.cpp
@@ -5,25 +5,40 @@ int main()
 {
     int n,q;
     cin>>n >>q;
+    // vector<int> a(n);
     int a[n];
-    for(int i =1; i<=n; i++)
+    for(int i =0; i<n; i++)
     {
         cin>>a[i];
     }
-
+    // sort(a.begin(),a.end());
+    sort(a,a+n);
     for(int i =0; i<q;i++)
     {
         int x;
         cin>>x;
         int flag =0;
-        for(int i =0; i<n; i++)
+        //implemt binary search
+        int l = 0;
+        int r = n-1;
+
+        while(l<=r)
         {
-            if(a[i]==x)
+            int mid = (l+r)/2;
+            if(a[mid]==x)
             {
-                flag =1;
+                flag = 1;
+                break;
+            }
+            else if(a[mid] > x){
+                r = mid-1;
+            }
+            else{
+                l = mid+1;
             }
         }
-        if(flag){
+
+        if(flag==1){
             cout<<"found"<<endl;
         }else{
             cout <<"not found"<<endl;
