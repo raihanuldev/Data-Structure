f7c576ecf68ab545d2653fb91f59c91a75759566
raihanuldev <rihanthedev@gmail.com>
2024-12-16 17:52:04 +0600

    insert_node_at_Head

---
+++ module-6 LinkedList Operation/Head_Insertion.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+        Node(int val)
+        {
+            this->val = val;
+            this->next = NULL;
+        }
+};
+
+void insert_at_head(Node* &head,int val){
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+
+}
+
+void printing_node(Node* head)
+{
+    Node* temp = head;
+    while(temp !=NULL)
+    {
+        cout<<temp->val<<endl;
+        temp = temp->next; 
+    }
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+    //connection
+    head->next = a;
+    a->next = tail;
+    //insert at head
+    insert_at_head(head,300);
+    printing_node(head);
+    return 0;
+}
