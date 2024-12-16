52237bff59e12505975715407f759fc663706661
raihanuldev <rihanthedev@gmail.com>
2024-12-16 20:21:01 +0600

    tail_complexity_optimized

---
+++ module-6 LinkedList Operation/insert_tail_optimized.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* next;
+        Node(int val)
+        {
+            this->val = val;
+            this->next = NULL;
+        }
+};
+
+void insert_at_tail(Node* &head,Node* &tail,int value)
+{
+    Node* newNode = new Node(value);
+    if(head==NULL)
+    {
+        head = newNode;
+        return;
+    }
+    tail->next=newNode;
+    tail = newNode;
+    
+}
+
+void printing_node(Node* head)
+{
+    Node* temp = head;
+    while(temp !=NULL)
+    {
+        cout<<(*temp).val<<endl;
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+    //connection
+    head->next = a;
+    a->next = tail;
+    //insert er pala
+    insert_at_tail(head,tail,100);
+    insert_at_tail(head,tail,200);
+    printing_node(head);
+    return 0;
+}
