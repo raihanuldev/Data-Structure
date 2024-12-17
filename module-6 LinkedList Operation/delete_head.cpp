a2c00f8cdaaac8d6f8a907951cc847d5f5939c7b
raihanuldev <rihanthedev@gmail.com>
2024-12-17 23:44:36 +0600

    deleteNode at Head

---
+++ module-6 LinkedList Operation/delete_head.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* next;
+        Node(int val)
+        {
+            this->val = val;
+            this->next = NULL;
+        }
+};
+
+void insert_at_tail(Node* &head,Node* &tail,int value)
+{
+    Node* newNode = new Node(value);
+    if(head==NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next=newNode;
+    tail = newNode;
+    
+}
+
+void printing_node(Node* head)
+{
+    Node* temp = head;
+    while(temp !=NULL)
+    {
+        cout<<(*temp).val<<endl;
+        temp = temp->next;
+    }
+}
+void delete_head(Node* &head){
+    Node* deleteNode = head;
+    head = deleteNode->next;
+    delete deleteNode;
+}
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+    int val;
+    while(true)
+    {
+        cin>>val;
+        if(val == -1){
+            break;
+        }
+        insert_at_tail(head,tail,val);
+    }
+    delete_head(head);
+    delete_head(head);
+    printing_node(head);
+    return 0;
+}
