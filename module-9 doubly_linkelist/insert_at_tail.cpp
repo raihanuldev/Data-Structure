028fed3ccd25942783f75c566b4a3796331a829b
raihanuldev <rihanthedev@gmail.com>
2024-12-21 23:47:23 +0600

    insertatTail

---
+++ module-9 doubly_linkelist/insert_at_tail.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* prev;
+        Node* next;
+        Node(int val)
+        {
+            this->val = val;
+            this->next = NULL;
+            this->prev = NULL;
+        }
+};
+void insert_at_tail(Node* &tail,int val){
+    Node* newNode = new Node(val);
+    Node* temp = tail;
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+void printing_forward(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    //connection 
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    insert_at_tail(tail,5);
+    printing_forward(head);
+    
+    return 0;
+}
