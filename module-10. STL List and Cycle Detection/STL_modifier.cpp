aaf6e46ef9ad8e4400c606b27c68b6ec3fec187f
raihanuldev <rihanthedev@gmail.com>
2024-12-24 20:16:02 +0600

    insert

--- module-10. STL List and Cycle Detection/STL_modifier.cpp
+++ module-10. STL List and Cycle Detection/STL_modifier.cpp
@@ -6,6 +6,13 @@ int main()
     list<int>l ={10,20,30};
     list<int>l2;
     l2 = l;
+    l2.push_back(40);
+    l2.push_front(5);
+    l2.pop_back();
+    l2.pop_front();
+    //insert at specific positon
+    l2.insert(next(l2.begin(),2),16);
+    l2.insert(next(l2.begin(),2),18);
     for(int val:l2){
         cout<<val<<endl;
     }
