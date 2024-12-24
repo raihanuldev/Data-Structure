f878737a0514dda08ca8af7912b13b49bf8c903b
raihanuldev <rihanthedev@gmail.com>
2024-12-24 20:09:12 +0600

    asignOn LIst

---
+++ module-10. STL List and Cycle Detection/STL_modifier.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int>l ={10,20,30};
+    list<int>l2;
+    l2 = l;
+    for(int val:l2){
+        cout<<val<<endl;
+    }
+    return 0;
+}
