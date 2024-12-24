a1a8e1fb4ad274f7328147e6da7f210ad2d9168f
raihanuldev <rihanthedev@gmail.com>
2024-12-24 20:51:55 +0600

    operations

---
+++ module-10. STL List and Cycle Detection/STL_Opearations.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int>l={10,78,23,20,32,35,60,79,79,103};
+    
+    // l.remove(10);
+    // l.sort();
+    // l.sort(greater<int>());
+    // l.unique(); //make sure your list are sorted
+    // l.reverse();
+    // cout<<l.back();
+    
+    // for(int val:l)
+    // {
+    //     cout<<val<<endl;
+    // }
+    return 0;
+}
