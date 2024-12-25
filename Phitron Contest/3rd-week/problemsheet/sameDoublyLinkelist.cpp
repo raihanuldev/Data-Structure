838f55d28ded743a5adb709bf6d479d5c747bf74
raihanuldev <rihanthedev@gmail.com>
2024-12-25 16:00:02 +0600

    checkif duplicate or NOT

--- Phitron Contest/3rd-week/problemsheet/sameDoublyLinkelist.cpp
+++ Phitron Contest/3rd-week/problemsheet/sameDoublyLinkelist.cpp
@@ -27,6 +27,23 @@ void inserting_tail(Node* &head,Node* &tail,int val)
     temp->next = newNode;
     tail = newNode;
 }
+bool check(Node* fristHead,Node* secHead)
+{
+    Node* tmp1 = fristHead;
+    Node* tmp2 = secHead;
+
+    while(tmp1 !=NULL && tmp2 !=NULL){
+        if(tmp1->val != tmp2->val){
+            return false;
+        }
+        tmp1 = tmp1->next;
+        tmp2 = tmp2->next;
+    }
+    if (tmp1 != NULL || tmp2 != NULL) {
+        return false;
+    }
+    return true;
+}
 
 void printing_forward(Node* head){
     Node* temp = head;
@@ -56,8 +73,13 @@ int main()
         if(y==-1){
             break;
         }
-        inserting_tail(sec_head,sec_tail,x);
+        inserting_tail(sec_head,sec_tail,y);
     }
-    printing_forward(frist_head);
+    // printing_forward(frist_head);
+    if (check(frist_head, sec_head)) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
     return 0;
 }
