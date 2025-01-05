3c61f60724c335fdaf2af5fa456339561c258e37
raihanuldev <rihanthedev@gmail.com>
2025-01-05 07:20:24 +0600

    STL list

---
+++ module-13 Stack/Stack_implementation_STL_LIST.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myStack{
+    public:
+        list<int>v; // for linklist = list for vector=> vector<>
+
+        void push(int value){
+            v.push_back(value);
+        }
+        void pop(){
+            v.pop_back();
+        }
+        int top(){
+            return v.back();
+        }
+        int size (){return v.size();}
+        bool empty()
+        {
+            return v.empty();
+        }
+
+};
+
+int main()
+{
+    myStack st;
+    
+    int n;cin>>n;
+    while(n--){
+        int x;cin>>x;
+        st.push(x);
+    }
+
+    // cout<<st.top()<<endl;
+    while(!st.empty()){
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+
+    return 0;
+}
