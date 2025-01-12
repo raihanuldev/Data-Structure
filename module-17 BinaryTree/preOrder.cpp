0ea648c12aba1233ebb937bd7faeb5b984d1f083
raihanuldev <rihanthedev@gmail.com>
2025-01-12 15:04:34 +0600

    preOrder

---
+++ module-17 BinaryTree/preOrder.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        Node(int val)
+        {
+            this->val = val;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+void preOrder(Node* root)
+{   if(root==NULL){
+        return;
+}
+    cout<<root->val<<" "; //root vlaue print mroebe
+    preOrder(root->left); //left er jonno print kore nia asbe
+    preOrder(root->right); //right er jonno nia asbe
+}
+
+int main()
+{
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+    
+    //build connection
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    b->right = c;
+
+    preOrder(root);
+
+    return 0;
+}
