4c1cd9d746074e3c07ed2d6008ccb8550c26bd24
raihanuldev <rihanthedev@gmail.com>
2025-01-20 15:45:14 +0600

    heap

---
+++ module-22 Heap/insert_at_min_heap.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    int val;
+    cin >> val;
+    v.push_back(val);
+    int curr_idx = v.size() - 1;
+    while (curr_idx!=0)
+    {
+        int par_idx = (curr_idx - 1) / 2;
+        if (v[par_idx] > v[curr_idx])
+        {
+            swap(v[par_idx], v[curr_idx]);
+        }
+        else
+        {
+            break;
+        }
+        curr_idx = par_idx;
+    }
+    for(int x:v){
+        cout<<x<<" ";
+    }
+    return 0;
+}
